80ec3cf0289a7f3acd5487a1f0a6fbdcc87a0fef
tosw164 <tosw164@aucklanduni.ac.nz>
2018-08-31 19:07:06 +1200

    Added KBest and changed tabs to space

--- World_Cup_2018/worldcup.py
+++ World_Cup_2018/worldcup.py
@@ -12,7 +12,7 @@ from sklearn.datasets import make_moons
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.linear_model import Ridge
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, classification_report
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
@@ -21,7 +21,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import mean_squared_error, r2_score
-
+from sklearn.feature_selection import SelectKBest, f_regression
 
 TEST_PERCENTAGE = 0.2
 MAX_PERCENTAGE = 100
@@ -35,172 +35,139 @@ TREES = "TREES"
 
 
 def setup(): #preprocessing
-	worldcup_information = pd.read_csv(FILE_PATH)
-	worldcup_information.drop(['Date', 'Location', 'Phase', 'Team1', 'Team1_Continent', 'Team2', 'Team2_Continent', 'Normal_Time'], axis=1, inplace=True)
+    worldcup_information = pd.read_csv(FILE_PATH)
+    worldcup_information.drop(['Date', 'Location', 'Phase', 'Team1', 'Team1_Continent', 'Team2', 'Team2_Continent', 'Normal_Time'], axis=1, inplace=True)
 
-	wc_features = worldcup_information.iloc[:, np.arange(21)].copy()
-	wc_goals = worldcup_information.iloc[:,21].copy()
-	wc_result = worldcup_information.iloc[:,22].copy()
+    wc_features = worldcup_information.iloc[:, np.arange(21)].copy()
+    wc_goals = worldcup_information.iloc[:,21].copy()
+    wc_result = worldcup_information.iloc[:,22].copy()
 
-	# Obtained from tutorial code
-	class DataFrameSelector(BaseEstimator, TransformerMixin):
-	    def __init__(self, attribute_names):
-	        self.attribute_names = attribute_names
-	    def fit(self, X, y=None):
-	        return self
-	    def transform(self, X):
-	        return X[self.attribute_names].values
+    # Obtained from tutorial code
+    class DataFrameSelector(BaseEstimator, TransformerMixin):
+        def __init__(self, attribute_names):
+            self.attribute_names = attribute_names
+        def fit(self, X, y=None):
+            return self
+        def transform(self, X):
+            return X[self.attribute_names].values
 
 
-	full_pipe = Pipeline([
-			('selector', DataFrameSelector(list(wc_features))),
-			('imputer', Imputer(strategy='median')),
-			('std_scaler', StandardScaler()),
-		])
+    full_pipe = Pipeline([
+            ('selector', DataFrameSelector(list(wc_features))),
+            ('imputer', Imputer(strategy='median')),
+            ('std_scaler', StandardScaler()),
+        ])
 
-	wc_result.reset_index(drop=True, inplace=True)
+    wc_result.reset_index(drop=True, inplace=True)
 
-	feature_prep = pd.DataFrame(data=full_pipe.fit_transform(wc_features), index=np.arange(1,65))
-	feature_prep.reset_index(drop=True, inplace=True)
+    feature_prep = pd.DataFrame(data=full_pipe.fit_transform(wc_features), index=np.arange(1,65))
+    feature_prep.reset_index(drop=True, inplace=True)
 
-	worldcup = pd.concat([feature_prep, wc_goals, wc_result.to_frame()], axis=1)
+    worldcup = pd.concat([feature_prep, wc_goals, wc_result.to_frame()], axis=1)
 
-	return worldcup
+    return worldcup
 
 def world_cup_classification(type):
-	worldcup = setup()
+    worldcup = setup()
 
-	x = worldcup.iloc[:,:20]
-	y = worldcup['Match_result']
+    x = worldcup.iloc[:,:20]
+    y = worldcup.iloc[:, -1]
 
-	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=TEST_PERCENTAGE)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=TEST_PERCENTAGE)
 
-	if type == PERCEPTRON:
-		#=========================================================================
-		## Perceptron
-		#=========================================================================
-		params = {
-			'alpha':[10**x for x in range(-10,1)],
-			'tol': [None],
-			'max_iter': [x for x in range(1,5)],
-		}
+    if type == PERCEPTRON:
+        params = {
+            'alpha':[10**x for x in range(-10,1)],
+            'tol': [None],
+            'max_iter': [x for x in range(1,5)],
+        }
 
-		grid_search_cv = GridSearchCV(Perceptron(), params)
-		grid_search_cv.fit(x_train, y_train)
-		print(grid_search_cv.best_estimator_)
+        grid_search_cv = GridSearchCV(Perceptron(), params)
 
-		y_pred = grid_search_cv.predict(x_test)
-		print("The prediction accuracy using the Perceptron is : {:.2f}%.".format(MAX_PERCENTAGE*accuracy_score(y_test, y_pred)))
+    elif type == NAIVE:
+        params = {}
 
-	elif type == NAIVE:
-		#=========================================================================
-		## Naive Baysian
-		#=========================================================================
-		params = {}
+        grid_search_cv = GridSearchCV(GaussianNB(), params)
 
-		grid_search_cv = GridSearchCV(GaussianNB(), params)
-		grid_search_cv.fit(x_train, y_train)
-		print(grid_search_cv.best_estimator_)
+    elif type == NEAR_NEIGHBOUR:
+        params = {
+            'n_neighbors': [x for x in range(2,10)],
+            'metric': ['minkowski','euclidean','manhattan'],
+        }
 
-		y_pred = grid_search_cv.predict(x_test)
-		print("The prediction accuracy using the Naive Baysean is : {:.2f}%.".format(MAX_PERCENTAGE*accuracy_score(y_test, y_pred)))
+        grid_search_cv = GridSearchCV(KNeighborsClassifier(), params)
 
-	elif type == NEAR_NEIGHBOUR:
-		#=========================================================================
-		## Nearest Neighbour
-		#=========================================================================
-		params = {
-			'n_neighbors': [x for x in range(2,10)],
-			'metric': ['minkowski','euclidean','manhattan'],
-		}
+    elif type == TREES:
+        params = {  
+                    'max_leaf_nodes':list(range(2,3)),
+                    'min_samples_split': [5],
+                }
 
-		grid_search_cv = GridSearchCV(KNeighborsClassifier(), params)
-		grid_search_cv.fit(x_train, y_train)
-		print(grid_search_cv.best_estimator_)
+        grid_search_cv = GridSearchCV(DecisionTreeClassifier(), params)
 
-		y_pred = grid_search_cv.predict(x_test)
-		print("The prediction accuracy using the Nearest Neighbour is : {:.2f}%.".format(MAX_PERCENTAGE*accuracy_score(y_test, y_pred)))
+    elif type == SVM:
+        params = {
+            'C': [10**x for x in range(-1,3)],
+            'gamma': [10**x for x in range(-1,2)],
+        }
 
-	elif type == TREES:
-		#=========================================================================
-		## Decision Tree Classification
-		#=========================================================================
-		params = {	
-					'max_leaf_nodes':list(range(2,3)),
-					'min_samples_split': [5],
-				}
+        grid_search_cv = GridSearchCV(SVC(), params)
 
-		grid_search_cv = GridSearchCV(DecisionTreeClassifier(), params)
-		grid_search_cv.fit(x_train, y_train)
-		print(grid_search_cv.best_estimator_)
+    grid_search_cv.fit(x_train, y_train)
 
-		y_pred = grid_search_cv.predict(x_test)
-		print("The prediction accuracy using the decision tree is : {:.2f}%.".format(MAX_PERCENTAGE*accuracy_score(y_test, y_pred)))
-
-
-	elif type == SVM:
-		#=========================================================================
-		## SVM
-		#=========================================================================
-		params = {
-			'C': [10**x for x in range(-1,3)],
-			'gamma': [10**x for x in range(-1,2)],
-		}
-
-		grid_search_cv = GridSearchCV(SVC(), params)
-		grid_search_cv.fit(x_train, y_train)
-		print(grid_search_cv.best_estimator_)
-
-		y_pred = grid_search_cv.predict(x_test)
-		print("The prediction accuracy using the SVM is : {:.2f}%.".format(MAX_PERCENTAGE*accuracy_score(y_test, y_pred)))
+    y_pred = grid_search_cv.predict(x_test)
+    print(classification_report(y_test, y_pred))
 
 
 def world_class_regression():
-	worldcup = setup()
+    worldcup = setup()
+    worldcup.drop(['Match_result'], axis=1, inplace=True)
 
-	#=========================================================================
-	##  Regression
-	#=========================================================================
-	worldcup.drop(['Match_result'], axis=1, inplace=True)
+    y = worldcup['Total_Scores']
+    x = SelectKBest(f_regression, k=19).fit_transform(worldcup.iloc[:,:20], y)
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+    #=========================================================================
+    ## Ridge Regression
+    #=========================================================================
+    reg = Ridge(alpha = .5)
+
+    reg.fit(x_train, y_train)
+
+    y_pred = reg.predict(x_test)
+    y_train_pred = reg.predict(x_train)
+    print('--------------Ridge---------------')
+    print('Coefficients and Intercept are: ', reg.coef_,"   ",reg.intercept_,' respectively')
+    print("Mean squared error for testing data: %.2f"
+          % mean_squared_error(y_test, y_pred))
+    print('Variance score for testing data: %.2f' % r2_score(y_test, y_pred))
+    print("Mean squared error for training data: %.2f"
+          % mean_squared_error(y_train, y_train_pred))
+    print('Variance score for training data: %.2f' % r2_score(y_train, y_train_pred))
+
+    #=========================================================================
+    ## Ordinary Regression
+    #=========================================================================
+    reg = LinearRegression()
+    reg.fit(x_train, y_train)
+
+    y_pred = reg.predict(x_test)
+    y_train_pred = reg.predict(x_train)
+
+    print('--------------Ordinary---------------')
+    print('Coefficients and Intercept are: ', reg.coef_,"   ",reg.intercept_,' respectively')
+    print("Mean squared error for testing data: %.2f"
+          % mean_squared_error(y_test, y_pred))
+    print('Variance score for testing data: %.2f' % r2_score(y_test, y_pred))
+    print("Mean squared error for training data: %.2f"
+          % mean_squared_error(y_train, y_train_pred))
+    print('Variance score for training data: %.2f' % r2_score(y_train, y_train_pred))
 
 
-	x = worldcup.iloc[:,:20]
-	y = worldcup['Total_Scores']
-
-	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
-
-	#=========================================================================
-	## Ridge Regression
-	#=========================================================================
-	reg = Ridge(alpha = .5)
-
-	reg.fit(x_train, y_train)
-
-	y_pred = reg.predict(x_test)
-	y_train_pred = reg.predict(x_train)
-	print('--------------Ridge---------------')
-	print('Coefficients and Intercept are: ', reg.coef_,"   ",reg.intercept_,' respectively')
-	print("Mean squared error for testing data: %.2f"
-	      % mean_squared_error(y_test, y_pred))
-	print('Variance score for testing data: %.2f' % r2_score(y_test, y_pred))
-	print("Mean squared error for training data: %.2f"
-	      % mean_squared_error(y_train, y_train_pred))
-	print('Variance score for training data: %.2f' % r2_score(y_train, y_train_pred))
-
-	#=========================================================================
-	## Ordinary Regression
-	#=========================================================================
-	reg = LinearRegression()
-	reg.fit(x_train, y_train)
-
-	y_pred = reg.predict(x_test)
-	y_train_pred = reg.predict(x_train)
-
-	print('--------------Ordinary---------------')
-	print('Coefficients and Intercept are: ', reg.coef_,"   ",reg.intercept_,' respectively')
-	print("Mean squared error for testing data: %.2f"
-	      % mean_squared_error(y_test, y_pred))
-	print('Variance score for testing data: %.2f' % r2_score(y_test, y_pred))
-	print("Mean squared error for training data: %.2f"
-	      % mean_squared_error(y_train, y_train_pred))
-	print('Variance score for training data: %.2f' % r2_score(y_train, y_train_pred))
+world_cup_classification(PERCEPTRON)
+world_cup_classification(NAIVE)
+world_cup_classification(NEAR_NEIGHBOUR)
+world_cup_classification(TREES)
+world_cup_classification(SVM)
+world_class_regression()
