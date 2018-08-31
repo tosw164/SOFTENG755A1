d664da11b04022183f05727ae9d96eb600d18fbb
tosw164 <tosw164@aucklanduni.ac.nz>
2018-08-31 19:30:02 +1200

    Added iteration count for landsat

--- Landsat/landsat.py
+++ Landsat/landsat.py
@@ -17,6 +17,7 @@ from sklearn.linear_model import Perceptron
 from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import mean_squared_error, classification_report
 
 TEST_PERCENTAGE = 0.2
 MAX_PERCENTAGE = 100
@@ -26,127 +27,101 @@ PERCEPTRON = "PERCEPTRON"
 NAIVE = "NAIVE"
 NEAR_NEIGHBOUR = "NEAR_NEIGHBOUR"
 SVM = "SVM"
-TREES = "TREES"
+TREES = "DECISION TREES"
 
 def setup():
-	sat_information = pd.read_csv(FILE_PATH, header=-1)
+    sat_information = pd.read_csv(FILE_PATH, header=-1)
 
-	s_features = sat_information.iloc[:, np.arange(36)].copy()
-	s_result = sat_information.iloc[:,36].copy()
+    s_features = sat_information.iloc[:, np.arange(36)].copy()
+    s_result = sat_information.iloc[:,36].copy()
 
-	# Obtained from tutorial code
-	class DataFrameSelector(BaseEstimator, TransformerMixin):
-	    def __init__(self, attribute_names):
-	        self.attribute_names = attribute_names
-	    def fit(self, X, y=None):
-	        return self
-	    def transform(self, X):
-	        return X[self.attribute_names].values
+    # Obtained from tutorial code
+    class DataFrameSelector(BaseEstimator, TransformerMixin):
+        def __init__(self, attribute_names):
+            self.attribute_names = attribute_names
+        def fit(self, X, y=None):
+            return self
+        def transform(self, X):
+            return X[self.attribute_names].values
 
 
-	full_pipe = Pipeline([
-			('selector', DataFrameSelector(list(s_features))),
-			('imputer', Imputer(strategy='median')),
-			('std_scaler', StandardScaler()),
-		])
+    full_pipe = Pipeline([
+            ('selector', DataFrameSelector(list(s_features))),
+            ('imputer', Imputer(strategy='median')),
+            ('std_scaler', StandardScaler()),
+        ])
 
-	s_result.reset_index(drop=True, inplace=True)
+    s_result.reset_index(drop=True, inplace=True)
 
-	feature_prep = pd.DataFrame(data=full_pipe.fit_transform(s_features), index=np.arange(1,6001))
-	feature_prep.reset_index(drop=True, inplace=True)
+    feature_prep = pd.DataFrame(data=full_pipe.fit_transform(s_features), index=np.arange(1,6001))
+    feature_prep.reset_index(drop=True, inplace=True)
 
-	sat = pd.concat([feature_prep, s_result.to_frame()], axis=1)
-	return sat
+    sat = pd.concat([feature_prep, s_result.to_frame()], axis=1)
+    return sat
 
 def landsat_classification(type):
-	sat = setup()
+    sat = setup()
 
-	x = sat.iloc[:,[0,1,3]]
-	y = sat[36]
+    ITERATIONS = 20
 
-	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=TEST_PERCENTAGE)
+    x = sat.iloc[:,:35]
+    y = sat[36]
 
-	if type == TREES:
-		#=========================================================================
-		## Decision Tree Classification
-		#=========================================================================
-		params = {	
-					'max_leaf_nodes':list(range(2,3)),
-					'min_samples_split': [5],
-				}
+    all_acc = []
 
-		grid_search_cv = GridSearchCV(DecisionTreeClassifier(random_state=42), params)
-		grid_search_cv.fit(x_train, y_train)
-		print(grid_search_cv.best_estimator_)
+    for i in range(ITERATIONS):
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=TEST_PERCENTAGE)
 
-		y_pred = grid_search_cv.predict(x_test)
-		print("The prediction accuracy using the decision tree is : {:.2f}%.".format(100*accuracy_score(y_test, y_pred)))
 
-	elif type == PERCEPTRON:
-		#=========================================================================
-		## Perceptron
-		#=========================================================================
-		params = {
-			'alpha':[10**x for x in range(-10,1)],
-			'tol': [None],
-			'max_iter': [x for x in range(1,5)],
-		}
+        if type == TREES:
+            params = {  
+                        'max_leaf_nodes':list(range(2,3)),
+                        'min_samples_split': [5],
+                    }
 
-		grid_search_cv = GridSearchCV(Perceptron(), params)
-		grid_search_cv.fit(x_train, y_train)
-		print(grid_search_cv.best_estimator_)
+            grid_search_cv = GridSearchCV(DecisionTreeClassifier(), params)
 
-		y_pred = grid_search_cv.predict(x_test)
-		print("The prediction accuracy using the Perceptron is : {:.2f}%.".format(100*accuracy_score(y_test, y_pred)))
+        elif type == PERCEPTRON:
+            params = {
+                'alpha':[10**x for x in range(-10,1)],
+                'tol': [None],
+                'max_iter': [x for x in range(1,5)],
+            }
 
-	elif type == NAIVE:
-		#=========================================================================
-		## Naive Baysian
-		#=========================================================================
-		params = {}
+            grid_search_cv = GridSearchCV(Perceptron(), params)
 
-		grid_search_cv = GridSearchCV(GaussianNB(), params)
-		grid_search_cv.fit(x_train, y_train)
-		print(grid_search_cv.best_estimator_)
+        elif type == NAIVE:
+            params = {}
 
-		y_pred = grid_search_cv.predict(x_test)
-		print("The prediction accuracy using the Naive Baysean is : {:.2f}%.".format(100*accuracy_score(y_test, y_pred)))
+            grid_search_cv = GridSearchCV(GaussianNB(), params)
 
-	elif type == NEAR_NEIGHBOUR:
-		#=========================================================================
-		## Nearest Neighbour
-		#=========================================================================
+        elif type == NEAR_NEIGHBOUR:
+            params = {
+                'n_neighbors': [x for x in range(2,10)],
+                'metric': ['minkowski','euclidean','manhattan'],
+            }
 
-		params = {
-			'n_neighbors': [x for x in range(2,10)],
-			'metric': ['minkowski','euclidean','manhattan'],
-		}
+            grid_search_cv = GridSearchCV(KNeighborsClassifier(), params)
 
-		grid_search_cv = GridSearchCV(KNeighborsClassifier(), params)
-		grid_search_cv.fit(x_train, y_train)
-		print(grid_search_cv.best_estimator_)
+        elif type == SVM:
+            params = {
+                # 'C': [10**x for x in range(-1,0)],
+                'C': [1],
+                # 'gamma': [10**x for x in range(-1,0)],
+                'gamma': [1],
+            }
 
-		y_pred = grid_search_cv.predict(x_test)
-		print("The prediction accuracy using the Nearest Neighbour is : {:.2f}%.".format(100*accuracy_score(y_test, y_pred)))
+            grid_search_cv = GridSearchCV(SVC(), params)
+        grid_search_cv.fit(x_train, y_train)
 
-	elif type == SVM:
-		#=========================================================================
-		## SVM
-		#=========================================================================
+        y_pred = grid_search_cv.predict(x_test)
+        all_acc.append(accuracy_score(y_test, y_pred))
 
-		params = {
-			'C': [10**x for x in range(-1,3)],
-			# 'C': [1],
-			'gamma': [10**x for x in range(-1,2)],
-			# 'gamma': [1],
-		}
+    print(type)
+    print(classification_report(y_test, y_pred))
+    print("The average accuracy over {} iterations is : {:.2f}%.".format(ITERATIONS, 100*(sum(all_acc)/ITERATIONS)))
 
-		grid_search_cv = GridSearchCV(SVC(), params)
-		grid_search_cv.fit(x_train, y_train)
-		print(grid_search_cv.best_estimator_)
 
-		y_pred = grid_search_cv.predict(x_test)
-		print("The prediction accuracy using the SVM is : {:.2f}%.".format(100*accuracy_score(y_test, y_pred)))
 
 landsat_classification(PERCEPTRON)
 landsat_classification(NAIVE)
