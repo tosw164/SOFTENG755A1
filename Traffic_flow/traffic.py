2ea48cd5015399342f113057c0d4c1076476acf6
tosw164 <tosw164@aucklanduni.ac.nz>
2018-08-31 19:54:35 +1200

    Added iteratioins and averaging for traffic

--- Traffic_flow/traffic.py
+++ Traffic_flow/traffic.py
@@ -16,19 +16,20 @@ import numpy as np
 from sklearn import datasets, linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import LinearRegression
+from sklearn.feature_selection import SelectKBest, f_regression
+
 
 
 traffic_information = pd.read_csv('traffic_flow_data.csv')
-print(traffic_information.describe())
 
 t_features = traffic_information.iloc[:, np.arange(450)].copy()
-# print(t_features)
 t_result = traffic_information.iloc[:,450].copy()
-# print(t_result)
 
-# exit()
-# Create a class to select numerical or categorical columns 
-# since Scikit-Learn doesn't handle DataFrames in this wise manner yet
+# Obtained from tutorial code
 class DataFrameSelector(BaseEstimator, TransformerMixin):
     def __init__(self, attribute_names):
         self.attribute_names = attribute_names
@@ -48,61 +49,51 @@ t_result.reset_index(drop=True, inplace=True)
 feature_prep = pd.DataFrame(data=pipe1.fit_transform(t_features), index=np.arange(1,7501))
 feature_prep.reset_index(drop=True, inplace=True)
 
-print(feature_prep.shape)
-print(t_result.to_frame().shape)
-
 traffic = pd.concat([feature_prep, t_result.to_frame()], axis=1)
 
-
-# print(traffic[:][:])
-# print(traffic.shape)
-
-x = traffic.iloc[:,:450]
 y = traffic['Segment23_(t+1)']
 
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+ITERATIONS = 10
+all_mse_ridge = []
+all_mse_ord = []
+all_var_ridge = []
+all_var_ord = []
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+for i in range(ITERATIONS):
+	x = SelectKBest(f_regression, k=400).fit_transform(traffic.iloc[:,:450], y)
 
-#=========================================================================
-## Ridge Regression
-#=========================================================================
-from sklearn.linear_model import Ridge
+	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
-reg = Ridge(alpha = .5)
+	#=========================================================================
+	## Ridge Regression
+	#=========================================================================
+	reg = Ridge(alpha = .5)
 
-reg.fit(x_train, y_train)
+	reg.fit(x_train, y_train)
 
-y_pred = reg.predict(x_test)
-y_train_pred = reg.predict(x_train)
+	y_pred = reg.predict(x_test)
+	y_train_pred = reg.predict(x_train)
+
+	all_mse_ridge.append(mean_squared_error(y_test, y_pred))
+	all_var_ridge.append(r2_score(y_test, y_pred))
+
+
+	#=========================================================================
+	## Ordinary Regression
+	#=========================================================================
+	reg = LinearRegression()
+	reg.fit(x_train, y_train)
+
+	y_pred = reg.predict(x_test)
+	y_train_pred = reg.predict(x_train)
+	all_mse_ord.append(mean_squared_error(y_test, y_pred))
+	all_var_ord.append(r2_score(y_test, y_pred))
+
+print('Number of iterations: ', ITERATIONS)
 print('--------------Ridge---------------')
-print('Coefficients and Intercept are: ', reg.coef_,"   ",reg.intercept_,' respectively')
-print("Mean squared error for testing data: %.2f"
-      % mean_squared_error(y_test, y_pred))
-print('Variance score for testing data: %.2f' % r2_score(y_test, y_pred))
-print("Mean squared error for training data: %.2f"
-      % mean_squared_error(y_train, y_train_pred))
-# Explained variance score: 1 is perfect prediction
-print('Variance score for training data: %.2f' % r2_score(y_train, y_train_pred))
-
-#=========================================================================
-## Ordinary Regression
-#=========================================================================
-from sklearn.linear_model import LinearRegression
-
-reg = LinearRegression()
-reg.fit(x_train, y_train)
-
-y_pred = reg.predict(x_test)
-y_train_pred = reg.predict(x_train)
+print("Mean squared error for testing data: %.2f" % (sum(all_mse_ridge)/ITERATIONS))
+print('Variance score for testing data: %.2f' % (sum(all_var_ridge)/ITERATIONS))
 
 print('--------------Ordinary---------------')
-print('Coefficients and Intercept are: ', reg.coef_,"   ",reg.intercept_,' respectively')
-print("Mean squared error for testing data: %.2f"
-      % mean_squared_error(y_test, y_pred))
-print('Variance score for testing data: %.2f' % r2_score(y_test, y_pred))
-print("Mean squared error for training data: %.2f"
-      % mean_squared_error(y_train, y_train_pred))
-# Explained variance score: 1 is perfect prediction
-print('Variance score for training data: %.2f' % r2_score(y_train, y_train_pred))
+print("Mean squared error for testing data: %.2f" % (sum(all_mse_ord)/ITERATIONS))
+print('Variance score for testing data: %.2f' % (sum(all_var_ord)/ITERATIONS))
